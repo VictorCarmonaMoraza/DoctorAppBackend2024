8565336612feaf3b3196e9414d71e2ca875de57e
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2024-11-28 21:01:00 +0100

    18-Actualizar la entidad usuario

--- Data/Migrations/20241128195926_AgregarPasswordEbtidadUser.Designer.cs
+++ Data/Migrations/20241128195926_AgregarPasswordEbtidadUser.Designer.cs
@@ -3,6 +3,7 @@ using Data.DbContext_Conection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241128195926_AgregarPasswordEbtidadUser")]
+    partial class AgregarPasswordEbtidadUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
